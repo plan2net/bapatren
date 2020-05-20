444af1c34c59cb4fd99e68ced4d0a8c8065e8cd5
Wolfgang Klinger <wk@plan2.net>
2020-05-20 13:21:41 +0200

    [TASK] Update compatibility to 9.5.12 - 9.5.18

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF['bapatren'] = [
     'version' => '9.5.14',
     'constraints' => [
         'depends' => [
-            'backend' => '9.5.12-9.5.17'
+            'backend' => '9.5.12-9.5.18'
         ],
         'conflicts' => [],
         'suggests' => []
